7958d7ef36ecbd881e13bc2a1ae4db95215a943a
Elana Hashman <elana.hashman@rackspace.com>
2016-06-02 16:31:19 -0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -39,34 +39,8 @@ setup(name='python-kadmin',
                   "src/PyKAdminCommon.c",
                   "src/PyKAdminXDR.c",
                   "src/getdate.c"
-                  ],
-              #extra_compile_args=["-O0"]
-              )
-          ],
-      classifiers=[
-          "Development Status :: 4 - Beta",
-          "Environment :: Console",
-          "Intended Audience :: System Administrators",
-          "Intended Audience :: Developers",
-          "Operating System :: POSIX",
-          "Programming Language :: C",
-          "Programming Language :: Python",
-          "Programming Language :: YACC",
-          "License :: OSI Approved :: MIT License",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-          "Topic :: System :: Systems Administration :: Authentication/Directory",
-          ]
-      )
-
-setup(name='python-kadmin-local',
-      version='0.1.1',
-      description='Python module for kerberos admin (kadm5) via root local interface',
-      url='https://github.com/russjancewicz/python-kadmin',
-      download_url='https://github.com/russjancewicz/python-kadmin/tarball/v0.1.1',
-      author='Russell Jancewicz',
-      author_email='russell.jancewicz@gmail.com',
-      license='MIT',
-      ext_modules=[
+              ],
+          ),
           Extension(
               "kadmin_local",
               libraries=["krb5", "kadm5srv", "kdb5"],
@@ -81,10 +55,10 @@ setup(name='python-kadmin-local',
                   "src/PyKAdminCommon.c",
                   "src/PyKAdminXDR.c",
                   "src/getdate.c"
-                  ],
+              ],
               define_macros=[('KADMIN_LOCAL', '')]
-              )
-          ],
+          )
+      ],
       classifiers=[
           "Development Status :: 4 - Beta",
           "Environment :: Console",
@@ -97,6 +71,5 @@ setup(name='python-kadmin-local',
           "License :: OSI Approved :: MIT License",
           "Topic :: Software Development :: Libraries :: Python Modules",
           "Topic :: System :: Systems Administration :: Authentication/Directory",
-          ]
-      )
-
+      ]
+)
