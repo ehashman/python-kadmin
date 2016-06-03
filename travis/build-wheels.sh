1be0674b884cf07d4cea5c7aab5c159ed78137e6
Elana Hashman <elana.hashman@rackspace.com>
2016-06-02 17:01:34 -0700

    Make auditwheel be verbose

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -17,7 +17,7 @@ done
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
-    auditwheel repair $whl -w /io/wheelhouse/
+    auditwheel -vv repair $whl -w /io/wheelhouse/
 done
 
 # Install packages and test
